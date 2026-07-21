7672a12f7f971c0a14723c340f05a2b9bea20088
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-07-21 18:47:34 +0200

    create get negative tests

--- src/test/java/api/getUser/UserIdNegativeTest.java
+++ src/test/java/api/getUser/UserIdNegativeTest.java
@@ -1,14 +1,39 @@
 package api.getUser;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.Test;
 
-class UserIdNegativeTest {
+import api.base.BaseApiTest;
 
+class UserIdNegativeTest extends BaseApiTest{
+
+	/**
+	 * Nem létező felhasználó lekérése.
+	 *
+	 * Ellenőrzi, hogy:
+	 * - a válasz státuszkódja 404 vagy 401,
+	 * - a válasz törzse üres objektum vagy null.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getUserIdUnsuccessfulTest() {
+		given().when().get("api/users/99").then().log().ifValidationFails()
+		.statusCode(anyOf(is(404), is(401))).body("$", anyOf( anEmptyMap(),nullValue()));
+	}
+	
+	/**
+	 * Létező felhasználó azonosítójának ellenőrzése.
+	 *
+	 * Ellenőrzi, hogy:
+	 * - a válasz státuszkódja 200,
+	 * - a felhasználó azonosítója nem 3.
+	 */
+	@Test
+	void checkUserIdTest() {
+		given().when().get("api/users/2").then().log().ifValidationFails()
+		.statusCode(200).body("data.id", not(equalTo(3)));
 	}
 
 }
